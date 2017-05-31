218bef0fe145b985c754f4705ce813a1e947bb9c
André Costa <andre.b.costa@accenture.com>
2017-05-31 17:23:35 +0100

    Fixed some files, starting the populate function

--- src/main.py
+++ src/main.py
@@ -1,20 +1,14 @@
 from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty
-from kivy.animation import Animation
-from kivy.metrics import dp
-from kivy.clock import Clock
-from kivy.lang import Builder
-from kivy.utils import get_color_from_hex
+from kivy.core.window import Window
+from kivy.config import Config
+from screens import CellCountRoot
 
-from kivymd.menu import MDDropdownMenu
-from kivymd.dialog import MDDialog
 from kivymd.theming import ThemeManager
 from kivymd.navigationdrawer import MDNavigationDrawer, NavigationLayout
-import kivymd.material_resources as m_res
 
-from license import license
-from screens import *
 from config import main_colors, languages
+
+Window.size = (1080/3, 1920/3)
 
 class CellCount(App):
     theme_cls = ThemeManager()
@@ -26,146 +20,27 @@ class CellCount(App):
     group_cells = languages[language]['group_cells']
 
     # def build(self):
+    #     #self.root = CellCountRoot()
     #     # Builder.load_file('cell_count.kv')
-    #     self.load_language()
+    # #     return CellCountRoot()
 
 
 class NavDrawer(MDNavigationDrawer):
     pass
 
 
-class CellCountRoot(NavigationLayout):
-    dots_menu = ObjectProperty()
-
-    def __init__(self, **kwargs):
-        super(CellCountRoot, self).__init__(**kwargs)
-        Clock.schedule_once(self.my_init)
-
-    def my_init(self, dt):
-        self.ids['toolbar'].dots_menu = DotsMenu(self)
-        #print self.ids['toolbar']
-        #self.ids['toolbar'].right_action_items = [['more-vert', lambda x: self.dots_menu.open(self.ids.right_actions)]]
-
-
-class ShowLicense(MDDialog):
-    link_label = ObjectProperty()
-
-    def __init__(self, **kwargs):
-        super(ShowLicense, self).__init__(**kwargs)
-        self.title = "License Information"
-        self.link_label.text = license
-        #self.content.bind(size=self.link_label.setter('text_size'))
-
-    def custom_open(self, menu):
-        menu.dismiss()
-        self.open()
-
-
-class DotsMenu(MDDropdownMenu):
-    def __init__(self, root, **kwargs):
-        super(DotsMenu, self).__init__(**kwargs)
-        self.items = [
-            {'viewclass': 'MDFlatButton',
-             'text': 'Licenses',
-             'on_release': lambda: self.menu_license.custom_open(self)},
-            {'viewclass': 'MDFlatButton',
-             'text': 'Settings',
-             'on_release': lambda: app.open_settings()}
-        ]
-        self.hor_growth = 'left'
-        self.ver_growth = 'down'
-        self.width_mult = 3
-        self.menu_license = ShowLicense()
-        self.control_size = (root.width - dp(3), root.top - dp(3))
-        root.bind(width=self.adapt_control_width)
-        root.bind(height=self.adapt_control_height)
-
-    def adapt_control_width(self, x, y):
-        self.control_size = (app.root.width - dp(3), self.control_size[1])
-        self.ids['md_menu'].right = self.control_size[0]
-
-    def adapt_control_height(self, x, y):
-        self.control_size = (self.control_size[0], app.root.top - dp(3))
-        self.ids['md_menu'].top = self.control_size[1]
-
-    # Over-ride Open Behaviour
-    def open(self, *largs):
-        Window.add_widget(self)
-        Clock.schedule_once(lambda x: self.display_menu(largs[0]), -1)
-
-    def display_menu(self, caller):
-        # We need to pick a starting point, see how big we need to be,
-        # and where to grow to.
-
-        # ---ESTABLISH INITIAL TARGET SIZE ESTIMATE---
-        target_width = self.width_mult * m_res.STANDARD_INCREMENT
-        # If we're wider than the Window...
-        if target_width > Window.width:
-            # ...reduce our multiplier to max allowed.
-            target_width = int(
-                Window.width / m_res.STANDARD_INCREMENT) * m_res.STANDARD_INCREMENT
-
-        target_height = sum([dp(48) for i in self.items])
-        # If we're over max_height...
-        if self.max_height > 0 and target_height > self.max_height:
-            target_height = self.max_height
-
-        # ---ESTABLISH VERTICAL GROWTH DIRECTION---
-        if self.ver_growth is not None:
-            ver_growth = self.ver_growth
-        else:
-            # If there's enough space below us:
-            if target_height <= self.control_size[1] - self.border_margin:
-                ver_growth = 'down'
-            # if there's enough space above us:
-            elif target_height < Window.height - self.control_size[1] - self.border_margin:
-                ver_growth = 'up'
-            # otherwise, let's pick the one with more space and adjust ourselves
-            else:
-                # if there's more space below us:
-                if self.control_size[1] >= Window.height - self.control_size[1]:
-                    ver_growth = 'down'
-                    target_height = self.control_size[1] - self.border_margin
-                # if there's more space above us:
-                else:
-                    ver_growth = 'up'
-                    target_height = Window.height - self.control_size[1] - self.border_margin
-
-        if self.hor_growth is not None:
-            hor_growth = self.hor_growth
-        else:
-            # If there's enough space to the right:
-            if target_width <= Window.width - self.control_size[0] - self.border_margin:
-                hor_growth = 'right'
-            # if there's enough space to the left:
-            elif target_width < self.control_size[0] - self.border_margin:
-                hor_growth = 'left'
-            # otherwise, let's pick the one with more space and adjust ourselves
-            else:
-                # if there's more space to the right:
-                if Window.width - self.control_size[0] >= self.control_size[0]:
-                    hor_growth = 'right'
-                    target_width = Window.width - self.control_size[0] - self.border_margin
-                # if there's more space to the left:
-                else:
-                    hor_growth = 'left'
-                    target_width = self.control_size[0] - self.border_margin
-
-        if ver_growth == 'down':
-            tar_y = self.control_size[1] - target_height
-        else:  # should always be 'up'
-            tar_y = self.control_size[1]
-
-        if hor_growth == 'right':
-            tar_x = self.control_size[0]
-        else:  # should always be 'left'
-            tar_x = self.control_size[0] - target_width
-        anim = Animation(x=tar_x, y=tar_y,
-                         width=target_width, height=target_height,
-                         duration=.3, transition='out_quint')
-        menu = self.ids['md_menu']
-        menu.pos = self.control_size
-        anim.start(menu)
+# class CellCountRoot(NavigationLayout):
+#     dots_menu = ObjectProperty()
+#
+#     def __init__(self, **kwargs):
+#         super(CellCountRoot, self).__init__(**kwargs)
+#         Clock.schedule_once(self.my_init)
+#
+#     def my_init(self, dt):
+#         print self.ids['scr_mngr'].get_screen('current_session').ids['toolbar']
+#
+#         #print self.ids['toolbar']
+#         #self.ids['toolbar'].right_action_items = [['more-vert', lambda x: self.dots_menu.open(self.ids.right_actions)]]
 
 
 if __name__ == '__main__':
--- src/screens.py
+++ src/screens.py
@@ -1,24 +1,14 @@
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.metrics import dp
 from kivy.clock import Clock, mainthread
 
-from kivymd.button import MDRaisedButton
-from kivymd.label import MDLabel
-from kivymd.list import ILeftBodyTouch, IRightBodyTouch
-from kivymd.selectioncontrols import MDCheckbox
-from kivymd.accordion import MDAccordionItem, MDAccordionSubItem
-
-from functools import partial
-
-
-class CellLine(ILeftBodyTouch, MDCheckbox):
-    pass
+from kivymd.navigationdrawer import NavigationLayout
+from kivymd.snackbar import Snackbar
+from custom_uix import DotsMenu
 
 class Workspace(Screen):
-    accordion = ObjectProperty()
     def __init__(self, **kwargs):
         super(Workspace, self).__init__(**kwargs)
         pass
@@ -31,23 +21,36 @@ class Workspace(Screen):
         #             first_level.add_widget(MDAccordionSubItem(text=item.keys()[0]))
         #         print type(item)
 
-    def j(self, x):
+    @staticmethod
+    def j(x):
         print x
         print 'click'
+
 
 class Theming(Screen):
     def __init__(self, **kwargs):
         super(Theming, self).__init__(**kwargs)
         pass
 
-class CurrentSession(Screen):
 
+class CellCountRoot(NavigationLayout):
+    def __init__(self, **kwargs):
+        super(CellCountRoot, self).__init__(**kwargs)
+        Clock.schedule_once(self.my_init)
+
+    def my_init(self, dt):
+        print self.ids
+        self.ids['toolbar'].dots_menu = DotsMenu(self)
+
+
+class CurrentSession(Screen):
     def __init__(self, **kwargs):
         super(CurrentSession, self).__init__(**kwargs)
 
     @mainthread
     def on_enter(self, *args):
-        self.parent.add_widget(self.parent.saved_screens['workspace']())
+        if not self.parent.has_screen('workspace'):
+            self.parent.add_widget(self.parent.saved_screens['workspace']())
 
 
 class Erithroblast(Screen):
@@ -64,7 +67,7 @@ class ScreenManagement(ScreenManager):
         super(ScreenManagement, self).__init__(**kwargs)
         self.saved_screens = {'theming': Theming, 'current_session': CurrentSession,
                               'workspace': Workspace}
-        #self.transition = FallOutTransition()
+        # self.transition = FallOutTransition()
         self.add_widget(CurrentSession())
         self.add_widget(Theming())
         # print Window
@@ -72,11 +75,12 @@ class ScreenManagement(ScreenManager):
         #     import android
         #     android.map_key(android.KEYCODE_BACK, 1001)
         Window.bind(on_keyboard=self.android_back_click)
-        #print self.screens
+        # print self.screens
 
-    def android_back_click(self, window, key, *largs):
+    @staticmethod
+    def android_back_click(window, key, *largs):
         print key
-        if key in [27, 1001, 1073742094, 4]: #1073742094:
+        if key in [27, 1001, 1073742094, 4]:  # 1073742094:
             # print (type(self.current_screen))
             # if self.has_screen('SingleNews') :
             #     if self.current_screen == self.get_screen('SingleNews'):
@@ -87,15 +91,22 @@ class ScreenManagement(ScreenManager):
             return True
 
     def change_screen(self, screen_name):
-        if self.has_screen(screen_name) == False:
-            #print self.saved_screens[screen_name]
+        if not self.has_screen(screen_name):
+            # print self.saved_screens[screen_name]
             self.add_widget(self.saved_screens[screen_name]())
         self.current = screen_name
+
 
 class SecondScreenManagement(ScreenManager):
     def __init__(self, **kwargs):
         super(SecondScreenManagement, self).__init__(**kwargs)
+        self.snack = Snackbar(text="Added to current session", duration=2)
 
-
-    def populate_workspace(self, _widget):
-        print _widget.ids
+    def populate_current_session(self, _widget, app):
+        #print _widget.ids
+        self.snack.show()
+        for cells in app.items[_widget.main][_widget.index][_widget.name]:
+            #print cells
+            if _widget.ids[cells].active:
+                print 'Adding ' + cells + ' to current session'
+        # print [x for x in _widget.ids['pick_layout'].walk()]
